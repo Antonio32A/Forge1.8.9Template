253e48bb7be7445e3327407fe4549d12c0607d7b
antonio32a <~@antonio32a.com>
2024-01-06 21:34:31 +0100

    Implement packet events

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,13 +1,26 @@
 package com.example;
 
-import net.minecraft.init.Blocks;
+import com.example.events.packet.PacketEvent;
+import com.example.events.packet.PacketHandler;
+import net.minecraft.network.play.client.C01PacketChatMessage;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "examplemod", useMetadata = true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        System.out.println("Dirt: " + Blocks.dirt.getUnlocalizedName());
+        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(PacketHandler.INSTANCE);
+    }
+
+    @SubscribeEvent
+    public void onPacketSent(PacketEvent.Sent event) {
+        if (event.getPacket() instanceof C01PacketChatMessage) {
+            C01PacketChatMessage packet = (C01PacketChatMessage) event.getPacket();
+            System.out.println("Sent message: " + packet.getMessage());
+        }
     }
 }
